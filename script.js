53359c9087226d8c6a0499a3935a62023d3a85f6
markisgod92 <markisgod92@gmail.com>
2024-07-31 09:48:54 +0200

    aggiunta modal e footer, modifiche codice

--- script.js
+++ script.js
@@ -10,16 +10,18 @@ const searchBtn = document.getElementById("searchBtn");
 const resultsContainer = document.getElementById("results-container");
 const buttonsContainer = document.getElementById("buttons-container")
 
-const getPexels = async (query = `query=sea`) => {
+const fetchPexels = async (url, query) => {
     try {
-        const response = await fetch(pexelsURL + query, {
+        const fullUrl = query ? `${url}${query}` : url;
+        const response = await fetch(fullUrl, {
             headers: {
                 Authorization: API_KEY
             }
-        });
+        })
         const data = await response.json();
         console.log(data);
-        displayImages(data)
+        displayImages(data);
+        toggleButtons(data);
     } catch (error) {
         console.log(error);
     }
@@ -28,14 +30,13 @@ const getPexels = async (query = `query=sea`) => {
 const searchImages = () => {
     if (searchInput.value !== "") {
         const query = `query=${searchInput.value}`;
-        getPexels(query);
+        fetchPexels(pexelsURL, query);
     }
 }
 
 const displayImages = async (data) => {
     resultsContainer.replaceChildren();
     data.photos.map(photo => createCard(photo));
-    createButtons(data);
 }
 
 const createCard = (data) => {
@@ -53,30 +54,44 @@ const createCard = (data) => {
     card.appendChild(img);
     col.appendChild(card);
     resultsContainer.appendChild(col);
+
+    card.addEventListener("click", () => displayModal(data));
 }
 
-const createButtons = (data) => {
-    buttonsContainer.replaceChildren();
+const handlePages = (pageUrl) => {
+    return () => {
+        if (pageUrl) {
+            fetchPexels(pageUrl);
+        };
+    };
+}
 
+const toggleButtons = (data) => {
     if (data.prev_page) {
-        const prevButton = document.createElement("button");
-        prevButton.setAttribute("class", "btn btn-secondary");
-        prevButton.innerText = "Previous";
-        prevButton.addEventListener("click", () => {
-            getPexels(data.prev_page.replace(pexelsURL, ""));
-        })
-        buttonsContainer.appendChild(prevButton);
+        document.getElementById("prevButton").disabled = false;
+        document.getElementById("prevButton").onclick = handlePages(data.prev_page);
+    } else {
+        document.getElementById("prevButton").disabled = true;
     }
     
     if (data.next_page) {
-        const nextButton = document.createElement("button");
-        nextButton.setAttribute("class", "btn btn-secondary");
-        nextButton.innerText = "Next";
-        nextButton.addEventListener("click", () => {
-            getPexels(data.next_page.replace(pexelsURL, ""));
-        })
-        buttonsContainer.appendChild(nextButton);
+        document.getElementById("nextButton").classList.disabled = false;
+        document.getElementById("nextButton").onclick = handlePages(data.next_page);
+    } else {
+        document.getElementById("nextButton").disabled = true;
     }
 }
 
-window.addEventListener("DOMContentLoaded", () => getPexels())
+const displayModal = (data) => {
+    const picModal = new bootstrap.Modal(document.getElementById('picModal'));
+    document.querySelector("#picModal .modal-content").style.backgroundColor = data.avg_color;
+    document.querySelector("#picModal img").src = data.src.large2x;
+    document.querySelector("#picModal img").alt = data.alt;
+    document.querySelector("#picModal a:first-of-type").href = data.photographer_url;
+    document.querySelector("#picModal a:first-of-type").innerText = data.photographer;
+    document.querySelector("#picModal a:last-of-type").href = data.url;
+    document.querySelector("#picModal a:last-of-type").innerText = "View on Pexels";
+    picModal.show();
+}
+
+window.addEventListener("DOMContentLoaded", () => fetchPexels(pexelsURL, `query=sea`))
